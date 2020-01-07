24b045aad14fd183a84a2c54c3c3742f9e599acf
Solwar <sacredglad@gmail.com>
2020-01-07 15:15:52 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+'use strict';
+let arr = ['plum.png', 'orange.jpg'] ;
+
+console.log(arr[0]);
+// alert('Hello!');
+
+// let answer = confirm('Are u ok?');
+
+//let answer = +prompt('Are u 18?', 'yes');// type plus if u want have number
+// in prompt
+
+// console.log(typeof(answer));
+
+//console.log("arr" + "sddj");
+
+// console.log(4 + "7");// it will be string
+// console.log(4 + +"7");// it will be number
+
+let incr = 10,
+    decr = 10;
+// incr++;
+// decr--;
+
+// console.log('incr: ' + ++incr); //prefix return changed var immediatly
+// console.log('decr: ' + --decr); //prefix return changed var immediatly
+
+// console.log('incr: ' + incr++); //postfix return old value
+// console.log('decr: ' + decr--); //postfix return old value
+
+// console.log(5%2);
+// console.log("2" == 2); // return TRUE!!!!
+// console.log("2" === 2); // return FALSE!!!!
+
+let isChecked = true,
+    isClose = true;
+
+console.log(isChecked && isClose); // AND
+console.log(isChecked || isClose); // OR
+console.log(!isChecked); // NOT
+
+
+
+
